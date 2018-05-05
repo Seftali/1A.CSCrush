7e5f70df4a5b33fa847112ac760e993a612ce8a8
TanerMan <taner.duzceer@ug.bilkent.edu.tr>
2018-05-05 18:56:59 +0300

    level movement count

--- project/CSCRUSH/src/cscrush/Level.java
+++ project/CSCRUSH/src/cscrush/Level.java
@@ -21,40 +21,40 @@ public class Level {
     private int movementCount(){
         switch (lev) {
             case 1:
-                count = 100;
+                count = 55;
                 target = 1000;
                 
                 break;
             case 2:
-                count = 90;
+                count = 50;
                 target = 2000;
                 break;
             case 3:
-                count = 80;
+                count = 45;
                 target = 3000;
                 break;
             case 4:
-                count = 70;
+                count = 40;
                 target = 4000;
                 break;
             case 5:
-                count = 60;
+                count = 35;
                 target = 5000;
                 break;
             case 6:
-                count = 50;
+                count = 30;
                 target = 6000;
                 break;
             case 7:
-                count = 40;
+                count = 25;
                 target = 7000;
                 break;
             case 8:
-                count = 30;
+                count = 20;
                 target = 8000;
                 break;
             case 9:
-                count = 20;
+                count = 15;
                 target = 9000;
                 break;
             default:
